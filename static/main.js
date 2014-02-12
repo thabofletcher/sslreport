aadc4928b8f7dee3fd269c1f424514c38afa8b94
Thabo <tha.bo.fletcher@gmail.com>
2014-02-12 10:27:32 -0800

    chart wired up, not reactive

--- static/main.js
+++ static/main.js
@@ -28,64 +28,48 @@ document.addEventListener('DOMContentLoaded', function() {
 		var child = childFb.val()
 		var table = document.getElementById("allresults");
 
-		table.innerHTML += '<th>' + child.date + '</th>' + '<th><a href=#' + child.id + '>' + child.id + '</a></th>' + '<th id="' + child.id + '">' + child.counts.http / 5 + '</th>'
+		table.innerHTML += 
+		'<th>' + child.date + '</th>' + 
+		'<th><a href=#' + child.id + '>' + child.id + '</a></th>' + 
+		'<th id="' + child.id + 'MITM">' + getMITM(child.counts) + '</th>' + 
+		'<th id="' + child.id + 'Q">' + getQ(child.counts) + '</th>'
 	})
 
 	fb.on('child_changed', function(childFb) {
 		var child = childFb.val()
-		document.getElementById(child.id).innerHTML = child.counts.http / 5
+		document.getElementById(child.id + "MITM").innerHTML = getMITM(child.counts)
+		document.getElementById(child.id + "Q").innerHTML = getQ(child.counts)
 	})
 
 	if (location.hash)
 		loadchart()
 })
 
+var getMITM = function(counts) {
+	if (counts.http + counts.https == 0) return '-'
+	return parseFloat(counts.http / (counts.http + counts.https) * 100).toPrecision(4) + '%'
+}
 
+var getQ = function(counts) {
+	return parseFloat((counts.ok + counts.error) / 5).toPrecision(4) + '%'
+}
 
 var loadchart = function() {
-
+	var data = []
 	var fb = new Firebase('https://sslreport.firebaseio.com/' + location.hash.substr(1));
 
-	// fb.on('child_added', function(childFb) {
-	// 	// <tr>
-	// 	// <th>Report Time</th>
-	// 	// <th>Report ID</th>
-	// 	// <th>Percent Vulnerable to MITM</th>
-	// 	// </tr>
-	// 	var child = childFb.val()
-	// 	var table = document.getElementById("allresults");
+	fb.once('value', function(childFb) {
+		var child = childFb.val()
 
-	// 	if (location.hash != child.id)
-	// 		table.innerHTML += '<th>' + child.date + '</th>' + '<th><a href=#' + child.id + '>' + child.id + '</a></th>' + '<th>' + child.counts.http / 5 + '</th>'
-	// })
-	var dataUrl = location.hash.substr(1)
+		data.push({value: child.counts.https, color:"blue"})
+		data.push({value: child.counts.http, color:"green"})
+		data.push({value: child.counts.error, color:"#F7464A"})
+		data.push({value: 500 - (child.counts.ok + child.counts.error), color:"#4D5360"})
 
-	var data = [
-		{
-			value: 30,
-			color:"#F7464A"
-		},
-		{
-			value : 50,
-			color : "#E2EAE9"
-		},
-		{
-			value : 100,
-			color : "#D4CCC5"
-		},
-		{
-			value : 40,
-			color : "#949FB1"
-		},
-		{
-			value : 120,
-			color : "#4D5360"
-		}
-	]
-
-	document.getElementById('chartholder').innerHTML = '<canvas id="chart" width="400" height="400"></canvas>'
-	var ctx = document.getElementById('chart').getContext('2d');
-	var chart = new Chart(ctx).Doughnut(data);
+		document.getElementById('chartholder').innerHTML = '<canvas id="chart" width="400" height="400"></canvas>'
+		var ctx = document.getElementById('chart').getContext('2d');
+		var chart = new Chart(ctx).Doughnut(data);
+	})
 }
 
 window.addEventListener('hashchange', loadchart, false)
